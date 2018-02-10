d9f510f0bb03b282f817bdacc40666dad32def27
povilas <p.vaitonis@gmail.com>
2018-02-10 21:48:11 +0200

    Added initial route set for posts

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -5,11 +5,28 @@ Vue.use(VueRouter);
 
 //routes
 export default new VueRouter({
-        routes: [{
-            path: '/',
-            name: 'home',
-            component: Vue.component('Home', require('./pages/Home.vue'))
-        },
+        routes: [
+            {
+                path: '/',
+                name: 'home',
+                component: Vue.component('Home', require('./pages/Home.vue'))
+            },
+            {
+                path: '/posts',
+                name: 'posts',
+                component: Vue.component('Posts', require('./pages/Posts.vue'))
+            },
+            {
+                path: '/posts/new',
+                name: 'newpost',
+                component: Vue.component('Blog', require('./pages/NewPost.vue'))
+            },
+            {
+                path: '/posts/:id',
+                name: 'post',
+                component: Vue.component('Post', require('./pages/Post.vue'))
+            },
+
         ]
     }
 )
